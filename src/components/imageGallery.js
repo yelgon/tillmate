0320ccbae68721312e0c9889fc4e32e4748e284d
yelgon <yelgon36@gmail.com>
2020-12-12 21:24:34 -0500

    imageGallery key moved

--- src/components/imageGallery.js
+++ src/components/imageGallery.js
@@ -6,8 +6,8 @@ export default function ImageGallery(props) {
       {/* used bootstrap to responsive web design */}
 
       {props.images.map((image, index) => (
-        <div className="card-body ml-3">
-          <img src={image.url} alt="picture" key={index} />
+        <div className="card-body ml-3" key={index}>
+          <img src={image.url} alt="picture" />
           {/* if there is no image alt will show in the place where the image would be */}
         </div>
       ))}

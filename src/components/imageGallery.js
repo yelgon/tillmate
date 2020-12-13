0a729aa68aaf232a327cf302f9b69880b5750179
yelgon <yelgon36@gmail.com>
2020-12-12 21:26:54 -0500

    images margin change

--- src/components/imageGallery.js
+++ src/components/imageGallery.js
@@ -6,7 +6,7 @@ export default function ImageGallery(props) {
       {/* used bootstrap to responsive web design */}
 
       {props.images.map((image, index) => (
-        <div className="card-body ml-3" key={index}>
+        <div className="card-body ml-1" key={index}>
           <img src={image.url} alt="picture" />
           {/* if there is no image alt will show in the place where the image would be */}
         </div>
